64fd04f670b5c69f5489ff6f1b221ba140b1fb7f
Angheloooo7 <vargasvillarroelm@gmail.com>
2023-10-18 11:50:45 -0400

    modelo y migracion realizado

--- database/migrations/2023_10_18_153049_create_personas_table.php
+++ database/migrations/2023_10_18_153049_create_personas_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('personas', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->integer('edad');
+            $table->integer('ci');
+            $table->text('correo',);
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('personas');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return "hola desde dla ruta principal";
 });
+
